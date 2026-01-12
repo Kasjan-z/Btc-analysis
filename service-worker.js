6d039281aa850eded3da734c78c40450083d1bc6
Casyan Kaine <kk.radulski@gmail.com>
2026-01-13 00:33:16 +0100

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'btc-meater-cache-v1';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalacja: cache podstawowych plików
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Aktywacja: czyszczenie starych cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch: najpierw sieć, fallback do cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
